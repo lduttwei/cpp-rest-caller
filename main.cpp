e32bec4ec4e7b404b44e21bb14014a6a55091ba6
Linus Duttweiler <linus.duttweiler@ergon.ch>
2021-09-09 20:36:39 +0200

    Parsing date_time with boost
    Boost easy removable

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cpr/cpr.h>
 #include <main.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
@@ -12,7 +13,8 @@ namespace rest
 {
 	cpr::Response Get()
 	{
-		return {};
+		cpr::Response r = cpr::Get(cpr::Url{url});
+		return r;
 	}
 
 	cpr::Response Get(char* id)
@@ -42,22 +44,37 @@ void print_response(cpr::Response r)
 {
 	std::cout << r.status_code << '\n';;                  // 200
 	//std::cout << r.header["content-type"] << '\n';       // application/json; charset=utf-8
-	//std::cout << r.text << '\n';   
+	std::cout << r.text << '\n';   
 	std::cout << r.url << '\n';
 }
 
+std::string parse_date_time(char* date, char* time)
+{
+	boost::gregorian::date d = boost::gregorian::from_string({date});
+	std::string res {to_iso_extended_string(d) + "T" + time + "Z"};
+	return res;
+}
 
 
 int main(int argc, char** argv)
 {
-	if ( argc > 1 )
-	{
-		char* id = argv[1];
-		auto r = rest::Get(id);
-		auto j = json::parse((r.text));
-		j.erase("id");
-		r = rest::Post(j);
-		print_response(r);
-	}
+	char* date = argv[1];
+	char* time = argv[2];
+	auto res = parse_date_time(date, time);
+	std::cout << res << '\n';
+
+	//auto r = rest::Get();
+	//print_response(r);
+	//auto j = json::parse((r.text));
+	//j.erase("id");
+	//time_t now;
+	//time(&now);
+	//char buf[sizeof "2011-10-08T07:08:09Z"];
+	//strftime(buf, sizeof buf, "%FT%TZ", gmtime(&now));
+	//json j;
+	//j["chrono"]={buf};
+	//std::cout << j.dump() << '\n';
+	//r = rest::Post(j);
+	//print_response(r);
 	return 0;
 }
